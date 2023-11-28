ae323689c2b04676cddcbb1f6e921ef15a8ed24b
xecgr <francesc.garcia.robert@gmail.com>
2023-11-28 07:09:58 +0100

    added property main.js

--- main.js
+++ main.js
@@ -6,18 +6,19 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 //https://openlayers.org/data/vector/us-states.json
 document.getElementById("btnMap").addEventListener('click',()=>{
 	let url = document.getElementById("geoJsonLink").value;
+	let property = document.getElementById("property").value;
 	url = url ? url : "https://openlayers.org/en/v4.6.5/examples/data/geojson/countries.geojson";
 	
 	fetch(url)
 	  .then(response => response.json())
 	  .then(fc => {
 			const mergeList = document.getElementById("mergeList").value.toLowerCase().split(",");
-			const countries = fc.features.filter(f => mergeList.includes(f.properties.name.toLowerCase()))
+			const _features = fc.features.filter(f => mergeList.includes(f.properties[property].toLowerCase()))
 
 			// do the union over each feature
-			let union = countries[0];
-			for (let i=1; i<countries.length; i++) {
-			  union = turf.union(union, countries[i]);
+			let union = _features[0];
+			for (let i=1; i<_features.length; i++) {
+			  union = turf.union(union, _features[i]);
 			}
 
 			const fc2 = {
